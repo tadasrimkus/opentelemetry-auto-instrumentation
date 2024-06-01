c217edbe5b77c00cc8a9f2241b0f9eb60f3b69f1
Tadas Rimkus <126669426+tadasrimkus@users.noreply.github.com>
2024-06-01 21:51:57 +0200

    Clean up code (#2)
    Co-authored-by: Tadas Rimkus <dev.tadas.rimkus@gmail.com>

--- src/Instrumentation/Extractor/InformationExtractorInterface.php
+++ src/Instrumentation/Extractor/InformationExtractorInterface.php
@@ -7,12 +7,12 @@ use OpenTelemetry\API\Trace\SpanInterface;
 interface InformationExtractorInterface
 {
 
-  public function supportsExtraction(object $object): bool;
+    public function supportsExtraction(object $object): bool;
 
-  public function extractInformation(object $object, SpanInterface $span, array $arguments = []): void;
+    public function extractInformation(object $object, SpanInterface $span, array $arguments = []): void;
 
-  public function supportsArgumentExtraction(mixed $argument): bool;
+    public function supportsArgumentExtraction(mixed $argument): bool;
 
-  public function extractArgumentInformation(mixed $argument, SpanInterface $span, int $counter): void;
+    public function extractArgumentInformation(mixed $argument, SpanInterface $span, int $counter): void;
 
 }
