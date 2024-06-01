d7fc7aae2e0c8f1953e53a6a2fd7cccd5b000007
Tadas Rimkus <dev.tadas.rimkus@gmail.com>
2024-06-01 21:48:33 +0200

    Clean up code

--- src/Instrumentation/DynamicInstrumentation.php
+++ src/Instrumentation/DynamicInstrumentation.php
@@ -5,6 +5,7 @@ namespace Buratinas\OpenTelemetryAutoInstrumentation\Instrumentation;
 use Buratinas\OpenTelemetryAutoInstrumentation\Instrumentation\Extractor\InformationExtractorAdditionInterface;
 use Buratinas\OpenTelemetryAutoInstrumentation\Instrumentation\Extractor\InformationExtractorChain;
 use Buratinas\OpenTelemetryAutoInstrumentation\Instrumentation\Extractor\InformationExtractorInterface;
+use Closure;
 use OpenTelemetry\API\Instrumentation\CachedInstrumentation;
 use OpenTelemetry\API\Trace\Span;
 use OpenTelemetry\API\Trace\SpanKind;
@@ -12,69 +13,72 @@ use OpenTelemetry\API\Trace\StatusCode;
 use OpenTelemetry\Context\Context;
 use OpenTelemetry\SemConv\TraceAttributes;
 
+use Throwable;
 use function OpenTelemetry\Instrumentation\hook;
 
 class DynamicInstrumentation
 {
-  public static null|InformationExtractorInterface|InformationExtractorAdditionInterface $extractor = NULL;
+    public static null|InformationExtractorInterface|InformationExtractorAdditionInterface $extractor = NULL;
 
-  public static function register(string $class, string $function, int $spanKind = SpanKind::KIND_SERVER): bool {
-    $instrumentation = new CachedInstrumentation('io.opentelemetry.contrib.php.symfony');
+    public static function register(string $class, string $function, int $spanKind = SpanKind::KIND_SERVER): bool
+    {
+        $instrumentation = new CachedInstrumentation('io.opentelemetry.contrib.php.symfony');
 
-    hook(
-      $class,
-      $function,
-      static::preClosure($instrumentation, $spanKind),
-      static::postClosure()
-    );
+        hook(
+            $class,
+            $function,
+            static::preClosure($instrumentation, $spanKind),
+            static::postClosure()
+        );
 
-    return TRUE;
-  }
+        return TRUE;
+    }
 
-  public static function preClosure(CachedInstrumentation $instrumentation, int $spanKind = SpanKind::KIND_SERVER): \Closure {
-    return static function (object $kernel, array $params, string $class, string $function, ?string $filename, ?int $lineno) use ($instrumentation, $spanKind) {
-      $spanName = sprintf('DI %s::%s', substr(strrchr($class, '\\'), 1), $function);
-      $span = $instrumentation->tracer()
-        ->spanBuilder($spanName)
-        ->setSpanKind($spanKind)
-        ->setAttribute(TraceAttributes::CODE_FUNCTION, $function)
-        ->setAttribute(TraceAttributes::CODE_NAMESPACE, $class)
-        ->setAttribute(TraceAttributes::CODE_FILEPATH, $filename)
-        ->setAttribute(TraceAttributes::CODE_LINENO, $lineno)
-        ->startSpan();
+    public static function preClosure(CachedInstrumentation $instrumentation, int $spanKind = SpanKind::KIND_SERVER): Closure
+    {
+        return static function (object $kernel, array $params, string $class, string $function, ?string $filename, ?int $lineno) use ($instrumentation, $spanKind) {
+            $spanName = sprintf('%s::%s', substr(strrchr($class, '\\'), 1), $function);
+            $span = $instrumentation->tracer()
+                ->spanBuilder($spanName)
+                ->setSpanKind($spanKind)
+                ->setAttribute(TraceAttributes::CODE_FUNCTION, $function)
+                ->setAttribute(TraceAttributes::CODE_NAMESPACE, $class)
+                ->setAttribute(TraceAttributes::CODE_FILEPATH, $filename)
+                ->setAttribute(TraceAttributes::CODE_LINENO, $lineno)
+                ->startSpan();
 
-      if (self::$extractor instanceof InformationExtractorInterface) {
-        self::$extractor->extractInformation($kernel, $span, $params);
-      }
+            static::getExtractor()?->extractInformation($kernel, $span, $params);
 
-      Context::storage()
-        ->attach($span->storeInContext(Context::getCurrent()));
-    };
-  }
+            Context::storage()->attach($span->storeInContext(Context::getCurrent()));
+        };
+    }
 
-  public static function postClosure(): \Closure {
-    return static function (object $kernel, array $params, $returnValue, ?\Throwable $exception) {
-      $scope = Context::storage()->scope();
-      if (!$scope) {
-        return;
-      }
-      $scope->detach();
-      $span = Span::fromContext($scope->context());
+    public static function postClosure(): Closure
+    {
+        return static function (object $kernel, array $params, $returnValue, ?Throwable $exception) {
+            $scope = Context::storage()->scope();
+            if (!$scope) {
+                return;
+            }
+            $scope->detach();
+            $span = Span::fromContext($scope->context());
 
-      if ($exception) {
-        $span->recordException($exception, [TraceAttributes::EXCEPTION_ESCAPED => TRUE]);
-        $span->setStatus(StatusCode::STATUS_ERROR, $exception->getMessage());
-      }
+            if ($exception) {
+                $span->recordException($exception, [TraceAttributes::EXCEPTION_ESCAPED => TRUE]);
+                $span->setStatus(StatusCode::STATUS_ERROR, $exception->getMessage());
+            }
 
-      $span->end();
-    };
-  }
+            $span->end();
+        };
+    }
 
-    public static function addExtractor(InformationExtractorInterface $extractor): void {
+    public static function addExtractor(InformationExtractorInterface $extractor): void
+    {
         static::getExtractor()->addExtractor($extractor);
     }
 
-    public static function getExtractor(): null|InformationExtractorInterface|InformationExtractorAdditionInterface {
+    public static function getExtractor(): null|InformationExtractorInterface|InformationExtractorAdditionInterface
+    {
         return static::$extractor ??= new InformationExtractorChain();
     }
 }
--- src/Instrumentation/Extractor/ObjectTypeExtractor.php
+++ src/Instrumentation/Extractor/ObjectTypeExtractor.php
@@ -7,31 +7,35 @@ use OpenTelemetry\API\Trace\SpanInterface;
 class ObjectTypeExtractor implements InformationExtractorInterface
 {
 
-  public function extractInformation(object $object, SpanInterface $span, array $arguments = []): void {
-    if ($this->supportsExtraction($object)) {
-      $span->setAttribute('object_type', gettype($object));
+    public function extractInformation(object $object, SpanInterface $span, array $arguments = []): void
+    {
+        if ($this->supportsExtraction($object)) {
+            $span->setAttribute('object_type', gettype($object));
+        }
     }
-  }
 
-  public function supportsExtraction(object $object): bool {
-    return TRUE;
-  }
-
-  public function supportsArgumentExtraction(mixed $argument): bool {
-    return TRUE;
-  }
-
-  public function extractArgumentInformation(mixed $argument, SpanInterface $span, int $counter): void {
-    if ($this->supportsArgumentExtraction($argument)) {
-      $argumentValue = match (gettype($argument)) {
-        'string', 'double', 'integer', 'boolean' => $argument,
-        'array' => json_encode($argument),
-        'object' => get_class($argument),
-        default => gettype($argument),
-      };
-
-      $span->setAttribute(sprintf('argument_%s', $counter), $argumentValue);
+    public function supportsExtraction(object $object): bool
+    {
+        return TRUE;
     }
-  }
+
+    public function supportsArgumentExtraction(mixed $argument): bool
+    {
+        return TRUE;
+    }
+
+    public function extractArgumentInformation(mixed $argument, SpanInterface $span, int $counter): void
+    {
+        if ($this->supportsArgumentExtraction($argument)) {
+            $argumentValue = match (gettype($argument)) {
+                'string', 'double', 'integer', 'boolean' => $argument,
+                'array' => json_encode($argument),
+                'object' => get_class($argument),
+                default => gettype($argument),
+            };
+
+            $span->setAttribute(sprintf('argument_%s', $counter), $argumentValue);
+        }
+    }
 
 }
