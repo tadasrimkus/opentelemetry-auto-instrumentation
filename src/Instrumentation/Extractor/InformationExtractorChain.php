2bfe0ef3869c6975dfa64107b19a8bf38fe167b9
Tadas Rimkus <dev.tadas.rimkus@gmail.com>
2024-06-01 21:48:33 +0200

    Clean up code

--- src/Instrumentation/Extractor/InformationExtractorChain.php
+++ src/Instrumentation/Extractor/InformationExtractorChain.php
@@ -4,46 +4,52 @@ namespace Buratinas\OpenTelemetryAutoInstrumentation\Instrumentation\Extractor;
 
 use OpenTelemetry\API\Trace\SpanInterface;
 
-class InformationExtractorChain implements InformationExtractorInterface, InformationExtractorAdditionInterface {
+class InformationExtractorChain implements InformationExtractorInterface, InformationExtractorAdditionInterface
+{
 
-  private array $extractors = [];
+    private array $extractors = [];
 
-  public function addExtractor(InformationExtractorInterface $extractor): void {
-    $this->extractors[] = $extractor;
-  }
+    public function addExtractor(InformationExtractorInterface $extractor): void
+    {
+        $this->extractors[] = $extractor;
+    }
 
-  public function extractInformation(object $object, SpanInterface $span, array $arguments = []): void {
-    /** @var InformationExtractorInterface $extractor */
-    foreach ($this->extractors as $extractor) {
-      if ($extractor->supportsExtraction($object)) {
-        $extractor->extractInformation($object, $span);
-      }
+    public function extractInformation(object $object, SpanInterface $span, array $arguments = []): void
+    {
+        /** @var InformationExtractorInterface $extractor */
+        foreach ($this->extractors as $extractor) {
+            if ($extractor->supportsExtraction($object)) {
+                $extractor->extractInformation($object, $span);
+            }
 
-      $counter = 0;
-      foreach ($arguments as $argument) {
-        if ($extractor->supportsArgumentExtraction($argument)) {
-          $extractor->extractArgumentInformation($argument, $span, $counter);
+            $counter = 0;
+            foreach ($arguments as $argument) {
+                if ($extractor->supportsArgumentExtraction($argument)) {
+                    $extractor->extractArgumentInformation($argument, $span, $counter);
+                }
+                $counter++;
+            }
         }
-        $counter++;
-      }
     }
-  }
 
-  public function supportsExtraction(object $object): bool {
-    return TRUE;
-  }
-
-  public function extractArgumentInformation(mixed $argument, SpanInterface $span, int $counter): void {
-    /** @var InformationExtractorInterface $extractor */
-    foreach ($this->extractors as $extractor) {
-      if ($extractor->supportsArgumentExtraction($argument)) {
-        $extractor->extractArgumentInformation($argument, $span, $counter);
-      }
+    public function supportsExtraction(object $object): bool
+    {
+        return TRUE;
     }
-  }
 
-  public function supportsArgumentExtraction(mixed $argument): bool {
-    return TRUE;
-  }
+    public function extractArgumentInformation(mixed $argument, SpanInterface $span, int $counter): void
+    {
+        /** @var InformationExtractorInterface $extractor */
+        foreach ($this->extractors as $extractor) {
+            if ($extractor->supportsArgumentExtraction($argument)) {
+                $extractor->extractArgumentInformation($argument, $span, $counter);
+            }
+        }
+    }
+
+    public function supportsArgumentExtraction(mixed $argument): bool
+    {
+        return TRUE;
+    }
 
 }
